0024949bc11b041dc4c7c89906818ff9087483d9
francesca <franchuporro@hotmail.com>
2019-06-05 19:08:33 -0300

    hice lo de proximamente

--- js/home.js
+++ js/home.js
@@ -9,7 +9,7 @@ window.onload = function() {
       for (var i = 0; i < peliculas.length; i++) {
         console.log(peliculas[i]);
 
-        document.querySelector(".destacdas").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+        document.querySelector(".destacadas").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
       }
     })
 }
@@ -20,12 +20,25 @@ fetch('https://api.themoviedb.org/3/movie/upcoming?page=1&language=en-US&api_key
   return respuesta.json()
 })
 .then(function(informacion) {
-  console.log(informacion);
-  //var peliculas = informacion.results
+  var peliculas = informacion.results
 
   for (var i = 0; i < peliculas.length; i++) {
     console.log(peliculas[i]);
 
     document.querySelector(".proximamente").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+  }
+})
+
+fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=&language=en-US&api_key=55e95e0127dd3fb1d9735ac5b1829c94')
+.then(function(respuesta) {
+  return respuesta.json()
+})
+.then(function(informacion) {
+  var peliculas = informacion.results
+
+  for (var i = 0; i < peliculas.length; i++) {
+    console.log(peliculas[i]);
+
+    document.querySelector(".puntaje").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
   }
 })
