6eadf47deceaad4664c5b6b744244156eaca5cec
SegundoUstariz <sustariz@udesa.edu.ar>
2019-06-05 16:56:50 -0300

    js

--- js/home.js
+++ js/home.js
@@ -13,3 +13,19 @@ window.onload = function() {
       }
     })
 }
+
+
+fetch('https://api.themoviedb.org/3/movie/upcoming?page=1&language=en-US&api_key=55e95e0127dd3fb1d9735ac5b1829c94')
+.then(function(respuesta) {
+  return respuesta.json()
+})
+.then(function(informacion) {
+  console.log(informacion);
+  //var peliculas = informacion.results
+
+  for (var i = 0; i < peliculas.length; i++) {
+    console.log(peliculas[i]);
+
+    document.querySelector(".proximamente").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+  }
+})
