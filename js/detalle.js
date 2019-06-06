e508160fa45987d616185bcbc14d5f2f1f5fabac
francesca <franchuporro@hotmail.com>
2019-06-06 16:34:17 -0300

    detalle.css y js

--- js/detalle.js
+++ js/detalle.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=55e95e0127dd3fb1d9735ac5b1829c94&page=1&language=en-US&page=1")
+  fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=55e95e0127dd3fb1d9735ac5b1829c94&page=1")
     .then(function(respuesta) {
       return respuesta.json()
     })
