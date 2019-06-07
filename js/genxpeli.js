f31e76b55a59a5a59eb73861092ef5caa591b83d
francesca <franchuporro@hotmail.com>
2019-06-07 13:52:17 -0300

    Conectamos el genero de la pelicula

--- js/genxpeli.js
+++ js/genxpeli.js
@@ -1,21 +1,25 @@
-window.addEventListener ("load", function () {
+window.addEventListener("load", function () {
   var queryString = location.search
 
   queryString = new URLSearchParams(queryString)
 
   var idGenero = queryString.get("id")
+  var nombreGenero = queryString.get("nombre")
+  document.querySelector("#genero")
 
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=​api_key=55e95e0127dd3fb1d9735ac5b1829c94&sort_by=popularity.des c&include_adult=true&include_video=true&page=1&with_genres=' + idGenero)
+  fetch('https://api.themoviedb.org/3/discover/movie?api_key=55e95e0127dd3fb1d9735ac5b1829c94&sort_by=popularity.des c&include_adult=true&include_video=true&page=1&with_genres=' + idGenero)
   .then(function(respuesta) {
     return respuesta.json()
   })
   .then(function(informacion) {
     var peliculas = informacion.results
+    console.log(peliculas);
 
     for (var i = 0; i < peliculas.length; i++) {
       console.log(peliculas[i]);
 
-      document.querySelector("#").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+      document.querySelector("#peliculasGenero").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+
     }
   })
-}
+})
