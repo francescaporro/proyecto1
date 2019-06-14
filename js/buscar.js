be3cdbb6b9be95e381ae800db8505e053a6c116b
SegundoUstariz <sustariz@udesa.edu.ar>
2019-06-14 16:54:20 -0300

    Footer + generos + detalles de una pelicula

--- js/buscar.js
+++ js/buscar.js
@@ -1,32 +1,30 @@
 window.addEventListener("load", function() {
 
-  var queryString = location.search
-
+  var queryString = window.location.search
   queryString = new URLSearchParams(queryString)
-
-  var buscar = queryString.get("buscar")
-
-  fetch('https://api.themoviedb.org/3/search/movie?api_key=55e95e0127dd3fb1d9735ac5b1829c94%26language&language=en-US&query=string&page=1&include_adult=false&region=string')
+  var buscado = queryString.get("buscar")
+console.log(buscado);
+  fetch("https://api.themoviedb.org/3/search/movie?api_key=55e95e0127dd3fb1d9735ac5b1829c94&language=en-US&query="+buscado+"&page=1&include_adult=false")
   .then(function(respuesta) {
     return respuesta.json()
   })
   .then(function(informacion) {
+    console.log(informacion);
     var peliculas = informacion.results
 
     for (var i = 0; i < peliculas.length; i++) {
 
-      document.querySelector(".proximamente").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
+      document.querySelector("main ul.buscado").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
     }
-  })
-  .then(function(respuesta) {
-    return respuesta.json()
-  })
-  .then(function(informacion) {
-    var peliculas = informacion.results
 
-    for (var i = 0; i < peliculas.length; i++) {
 
-      document.querySelector(".resultados").innerHTML += '<li> <a href="detalleDeUnaPelicula.html">   <img src="https://image.tmdb.org/t/p/original' + peliculas[i].poster_path + '" alt=""></a></li>'
-    }
   })
+  .catch (function (error) {
+    console.log("Error" + error);
+  })
+
+
+
+
+
 })
